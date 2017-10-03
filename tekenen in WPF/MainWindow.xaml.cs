ae206477cdc6be8cf44e571f03eb4b231cc6c091
Dante Budin <dante.budin@immalle.eu>
2017-10-03 09:58:44 +0200

    knopjes

--- tekenen in WPF/MainWindow.xaml.cs
+++ tekenen in WPF/MainWindow.xaml.cs
@@ -24,5 +24,44 @@ namespace tekenen_in_WPF
         {
             InitializeComponent();
         }
+
+        private void Knop1_Click(object sender, RoutedEventArgs e)
+        {
+            double top = 0;
+            double left = 0;
+
+            top = mijnRechthoek.Margin.Top;
+            left = mijnRechthoek.Margin.Left;
+
+            left++;
+
+            mijnRechthoek.Stroke = Brushes.Beige;
+            mijnRechthoek.Margin = new Thickness(left, top, 0, 0);
+        }
+
+            private void Knop2_MouseEnter(object sender, MouseEventArgs e)
+        {
+            MessageBox.Show("Hallo wereld!", "Hallo", MessageBoxButton.YesNo, MessageBoxImage.Asterisk);
+        }
+
+        private void Knop3_Click(object sender, RoutedEventArgs e)
+        {
+            Line lijn = new Line();
+
+            lijn.X1 = 10;
+            lijn.Y1 = 10;
+            lijn.X2 = 190;
+            lijn.Y2 = 190;
+            lijn.Stroke = new SolidColorBrush(Colors.DarkSalmon);
+
+            mijnCanvas.Children.Add(lijn);
+
+
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            mijnCanvas.Background = Brushes.DarkTurquoise;
+        }
     }
 }
